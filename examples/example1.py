2ff8e4f015d53983fd7b9111622a395060222ccc
Aschwin <aschwin.schilperoort@gmail.com>
2025-01-23 17:32:19 +0100

    add publish to pypi github action and pyproject.toml updates

--- examples/example1.py
+++ examples/example1.py
@@ -2,16 +2,20 @@ from skferm.datasets.synthetic import generate_synthetic_growth
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Parameters
-time = np.linspace(0, 24, 100)
-synthetic_data = generate_synthetic_growth(
-    time, model="logistic", N0=1e6, Nmax=1e9, r=0.3, noise_std=1e7
-)
+def main():
+    # Parameters
+    time = np.linspace(0, 24, 100)
+    synthetic_data = generate_synthetic_growth(
+        time, model="logistic", N0=1e6, Nmax=1e9, r=0.3, noise_std=1e7
+    )
 
-# Plot
-plt.plot(synthetic_data["time"], synthetic_data["population"], label="Synthetic Data")
-plt.xlabel("Time (hours)")
-plt.ylabel("Population (CFU/mL)")
-plt.title("Synthetic Microbial Growth Curve")
-plt.legend()
-plt.show()
+    # Plot
+    plt.plot(synthetic_data["time"], synthetic_data["population"], label="Synthetic Data")
+    plt.xlabel("Time (hours)")
+    plt.ylabel("Population (CFU/mL)")
+    plt.title("Synthetic Microbial Growth Curve")
+    plt.legend()
+    plt.savefig("synthetic_growth_curve.png")
+
+if __name__ == "__main__":
+    main()
